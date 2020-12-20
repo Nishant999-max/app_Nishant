5eb7d0fac6236225460ad8dd80cfee9986a16f53
Nishant999-max <nishant.sethu99@gmail.com>
2020-12-20 19:36:25 +0530

    update GET

--- Controllers/Student.cs
+++ Controllers/Student.cs
@@ -1,42 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace app.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class StudentController : ControllerBase
-    {
+    public class StudentController : ControllerBase{
         private static readonly List<Student>Students = new List<Student>(){
             new Student(){
+                Id = 1,
+                Name =  "Nishant",
+                Roll = 15,
+                DOB = new DateTime(1999,1,1),
+                age = 21
+                },
+            new Student(){
+               Id = 2,
+               Name = "Viknis",
+               Roll = 16,
+               DOB = new DateTime(1999,1,1),
+               age = 22
+            }
+        };
             
     
-        },
-        new Student(){
-            
-        }
-        }
+       
 
         [HttpGet]
-        public ActionResult<List<Student>> Get()
-        {  
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<Student> GetStudent(long id)
-        {   
-        }
-
-        [HttpPost]
-        public ActionResult<Student> Post(Student Student)
-        {   
-        }
-
-        [HttpPut]
-        public ActionResult<Student> Put(Student Student)
-        {
-        }
-
-        [HttpDelete]
-        public ActionResult<long> Delete(long id)
-        {
+        public List<Sudent> Get()
+        { 
+            return Students; 
         }
     }
+}
